6e70a575868f35780f9b201f52f74c9c73889450
Unknown <s16391@pja.edu.pl>
2018-01-04 16:40:34 +0100

    adding poi

--- app/src/main/java/com/example/maro/prj4and/AddPlace/AddPoiActivity.kt
+++ app/src/main/java/com/example/maro/prj4and/AddPlace/AddPoiActivity.kt
@@ -2,12 +2,16 @@ package com.example.maro.prj4and.AddPlace
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
+import com.example.maro.prj4and.Place.FirebaseDB
 import com.example.maro.prj4and.Place.Place
 import com.example.maro.prj4and.R
 import kotlinx.android.synthetic.main.activity_add_poi.*
@@ -16,7 +20,8 @@ import java.lang.Integer.parseInt
 
 class AddPoiActivity : AppCompatActivity() {
     var locationManager : LocationManager? = null
-    val PERMISSION_ACCESS_COARSE_LOCATION: Int = 99
+    val PERMISSION_ACCESS_FINE_LOCATION: Int = 99
+    var loc: Location? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,47 +30,51 @@ class AddPoiActivity : AppCompatActivity() {
         btnAdd.setOnClickListener { addPlaces() }
 
        setupPermissions()
-
-//        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
-//        val provider = locationManager!!.getBestProvider(Criteria(), false)
-//        val location = locationManager!!.getLastKnownLocation(provider)
-
-//        tvLocation.text = "" + location.longitude + ":" + location.latitude
-
-//        try {
-//            locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener);
-//        } catch(ex: SecurityException) {
-//            Log.d("myTag", "Security Exception, no location available");
-//        }
     }
 
     fun setupPermissions() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
-                    PERMISSION_ACCESS_COARSE_LOCATION)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    PERMISSION_ACCESS_FINE_LOCATION)
+        } else {
+            getLocation()
         }
 
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         when (requestCode) {
-            PERMISSION_ACCESS_COARSE_LOCATION -> if (!grantResults.isEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // All good!
+            PERMISSION_ACCESS_FINE_LOCATION -> if (!grantResults.isEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+               getLocation()
             } else {
                 Toast.makeText(this, "Need your location!", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-//    define the listener
-//    private val locationListener: LocationListener = object : LocationListener {
-//        override fun onLocationChanged(location: Location) {
-//            tvLocation.text = "" + location.longitude + ":" + location.latitude
-//        }
-//        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
-//        override fun onProviderEnabled(provider: String) {}
-//        override fun onProviderDisabled(provider: String) {}
-//    }
+    fun getLocation() {
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
+//        val provider = locationManager!!.getBestProvider(Criteria(), false)
+//        val location = locationManager!!.getLastKnownLocation(provider)
+
+//        tvLocation.text = "" + location.longitude + ":" + location.latitude
+
+        try {
+            locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0L, 0f, locationListener);
+        } catch(ex: SecurityException) {
+            Log.d("myTag", "Security Exception, no location available");
+        }
+    }
+
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            tvLocation.text = "Longitude: " + location.longitude + "\nLatitude: " + location.latitude
+            loc = location
+        }
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
 
     fun addPlaces() {
         if (etName.text.isEmpty() || etDesc.text.isEmpty() || etRadius.text.isEmpty()) {
@@ -82,8 +91,13 @@ class AddPoiActivity : AppCompatActivity() {
             return
         }
 
-        val place = Place(etName.text.toString(), etDesc.text.toString(), rad)
+        if (loc == null) {
+            val toast = Toast.makeText(this, "No location Data", 3)
+            toast.show()
+        }
 
-//        FirebaseDB.saveToFirebase(place)
+        val place = Place(etName.text.toString(), etDesc.text.toString(), rad, loc!!)
+
+        FirebaseDB.saveToFirebase(place)
     }
 }
--- app/src/main/java/com/example/maro/prj4and/Place/Place.kt
+++ app/src/main/java/com/example/maro/prj4and/Place/Place.kt
@@ -1,9 +1,12 @@
 package com.example.maro.prj4and.Place
 
+import android.location.Location
+
 /**
  * Created by maro on 03.01.2018.
  */
 data class Place(val name: String,
                  val desc: String,
-                 val radius: Int) {
+                 val radius: Int,
+                 val loc: Location) {
 }
